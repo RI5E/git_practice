f236efa98a055f691d4bb7ddb61ba3282cb6828d
[Michael <michael.fan@berkeley.edu>
2016-01-01 12:26:30 -0500

    This is the first push

---
+++ push_1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by MichaelFan on 1/1/2016.
+ */
+public class push_1 {
+    public static void main (String args [])
+    {
+        System.out.println("This is an example");
+        System.out.println("first commit ends here");
+    }
+}
