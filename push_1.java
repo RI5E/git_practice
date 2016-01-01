455f827609ccdd1584ccbaecb304bb2177e881f3
[Michael <michael.fan@berkeley.edu>
2016-01-01 13:42:52 -0500

    commit #3 this time with practice from hw0

--- push_1.java
+++ push_1.java
@@ -8,4 +8,18 @@ public class push_1 {
         System.out.println("first commit ends here");
         System.out.println("second commit here");
     }
+
+    /*
+    commit 3: exercise from hw0
+     */
+    public void drawTriangle(int n){
+        int i = 1;
+        while(i <= n){
+            int j = 1;
+            while(j <= i){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
